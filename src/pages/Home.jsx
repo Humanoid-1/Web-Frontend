7e007c7e53eba3e94e874b7dc4b442d52e437d2f
arman <arman@humanoid.com>
2025-08-06 17:56:51 +0530

    Initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import "./Home.css"; // import the custom CSS
+
+const categories = [
+  { name: "Dell", image: "/Dell.jpg" },
+  { name: "HP", image: "/hp.jpg" },
+  { name: "Asus", image: "/Asus.jpg" },
+  { name: "Apple", image: "/Apple.jpg" },
+  { name: "Lenovo", image: "/Lenovo.jpg" },
+  { name: "Acer", image: "/Acer.jpg" },
+  { name: "MSI", image: "/Msi.jpg" },
+  { name: "Samsung", image: "/Samsung.jpg" },
+];
+
+const Home = () => {
+  return (
+    <div className="home-container">
+      <h1 className="home-heading">Shop by Brands</h1>
+
+      <div className="brand-grid">
+        {categories.map((category, index) => (
+          <div className="brand-card" key={index}>
+            <div className="brand-image-container">
+              <img
+                src={category.image}
+                alt={category.name}
+                className="brand-image"
+              />
+            </div>
+            <span className="brand-name">{category.name}</span>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
+

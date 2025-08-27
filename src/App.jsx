b8c64b345ff0bfef380887cba51ce99dc33f112a
jaseem <jaseem@humanoidmaker.com>
2025-08-27 11:40:39 +0530

    code pull

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import Home from "./pages/Home";
 import Header from "./components/Header";
-import Footer from "./pages/footer";
 import { Route, Routes } from "react-router-dom";
 import About from "./pages/About";
 import ContactUs from "./pages/ContactUs";
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
 import BrandPage from "./pages/BrandPage";
-import Footer from "./pages/Footer";
+import Footer from "./components/Footer";
+import Accessories from "./pages/Accessories";
 
 function App() {
   return (

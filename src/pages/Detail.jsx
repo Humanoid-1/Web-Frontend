fc795280b46985bbbd367b06ac78f521490fe166
Aakif002 <aakif@humanoidmaker.com>
2025-09-13 17:45:42 +0530

    update code

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,6 +1,7 @@
 import React, { useRef, useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import "../pages/Detail.css";
+import axios from "axios";
 import { MdModeFanOff } from "react-icons/md";
 
 const Detail = () => {
@@ -36,15 +37,37 @@ useEffect(() => {
   if (loading) return <h2>Loading...</h2>;
   if (!laptop) return <h2>Laptop not found</h2>;
 
-  return (
-    <div>
-      <h2>{laptop.brand} {laptop.model}</h2>
-      <p>Price: ₹{laptop.price}</p>
-      <p>Ratings: {laptop.ratings} ⭐</p>
-      <p>Storage:{laptop.storage}</p>
-      <img src={selectedImage} alt={laptop.model} width="300" />
+return (
+   
+
+  <div className="detail-container">
+     
+     
+    <div className="detail-image-container">
+      <img 
+        src={selectedImage} 
+        alt={laptop.model} 
+        className="detail-image" 
+      />
     </div>
-  );
+
+   
+    
+    <div className="detail-info">
+      <h3><strong>{laptop.description}</strong></h3>
+      
+      <p><strong>Ratings:</strong> {laptop.ratings} ⭐</p>
+      <p><strong>model:</strong> {laptop.model}</p>
+      <p><strong>Price:</strong> ₹{laptop.price}</p>
+      
+    </div>
+    <div>
+      <button className="btn">Buy Now</button>
+    </div>
+   
+  </div>
+);
+
 };
 
 export default Detail;

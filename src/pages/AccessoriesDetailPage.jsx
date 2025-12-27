b7f0a513aee80321add7466d797165353b1968b1
Humanoid-1 <uzaif@humanoidmaker.com>
2025-12-27 18:19:52 +0530

    Updated Detail.css

--- src/pages/AccessoriesDetailPage.jsx
+++ src/pages/AccessoriesDetailPage.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState, useRef } from "react";
 import { useParams, useNavigate } from "react-router-dom";
-import "../pages/"
+import "../pages/Detail.css"
 
 function AccessoryDetailPage() {
   const { id, category } = useParams();
